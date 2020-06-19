233e45510f425bea0e274802c1a3a0d0873bcaf4
Phạm Hữu Thạnh Đạt <dathtp@gmail.com>
2020-06-19 15:49:02 +0700

    init project

---
+++ src/main/java/vn/inspiron/mcontract/McontractApplication.java
@@ -0,0 +1,13 @@
+package vn.inspiron.mcontract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McontractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McontractApplication.class, args);
+	}
+
+}
